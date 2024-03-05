a3f04a0200c69603ea93bd8f3df7f42bd35983fc
Iron-E <code.iron.e@gmail.com>
2024-03-05 18:39:46 -0500

    ci: use scabbard v0.8

--- ci/scope.ts
+++ ci/scope.ts
@@ -1,25 +1,4 @@
-import { BASE_DEPENDENCIES } from '@iron-e/scabbard/rust';
-import { CacheVolume, Container, Directory } from '@dagger.io/dagger';
-import { declare, inject, readIgnoreFile } from '@iron-e/scabbard';
+import { setTo } from '@iron-e/scabbard';
+import { CARGO_HACK_VERSION } from '@iron-e/scabbard/rust/scope/client';
 
-const exclude = await Array.fromAsync(readIgnoreFile());
-
-declare('cargoHome', client => client.cargoHomeCache());
-declare('project', client => client.host().directory('.'));
-
-declare('base', ['project'], client =>
-	client
-		.container()
-		.pipeline('install deps')
-		.fromWithDeps('rust:1.76.0-alpine', BASE_DEPENDENCIES)
-		.withWorkDirectory(inject('project').instance(Directory), { exclude }),
-);
-
-declare('withCargo', ['base', 'cargoHome'], _ =>
-	inject('base')
-		.instance(Container)
-		.pipeline('mount cargo cache')
-		.withCargoHome(inject('cargoHome').instance(CacheVolume))
-		.pipeline('install cargo-hack')
-		.withCargoInstall('cargo-hack@0.6.20')
-);
+setTo('0.6.20', CARGO_HACK_VERSION);
